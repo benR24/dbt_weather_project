0c055a73082bd7ca23e4488e05da5cd48caf2934
Benedict Reymann <benedict.reymann@gmail.com>
2024-11-11 10:30:44 +0000

    undo

--- models/staging/staging_airports.sql
+++
@@ -1,8 +0,0 @@
-sql
-    WITH airports_regions_join AS (
-        SELECT * 
-        FROM {{source('staging_flights', 'airports')}}
-        LEFT JOIN {{source('staging_flights', 'regions')}}
-        USING (country)
-    )
-    SELECT * FROM airports_regions_join
