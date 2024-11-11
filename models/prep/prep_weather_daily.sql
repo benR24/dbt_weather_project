54e07e7a81d47048abfd842a28a582d8fcb48fe3
benR24 <benedict.reymann@gmail.com>
2024-11-11 12:23:44 +0100

    prep1

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -4,21 +4,21 @@ WITH daily_data AS (
     ),
     add_features AS (
         SELECT *
-    		, ... AS date_day
-    		, ... AS date_month
-    		, ... AS date_year
-    		, ... AS cw
-    		, ... AS month_name
-    		, ... AS weekday
+    		, date_part('day', date) AS date_day
+    		, date_part('month', date) AS date_month
+    		, date_part('year', date) AS date_year
+    		, EXTRACT(week from date) AS cw
+    		, EXTRACT(month from date) AS month_name
+    		, EXTRACT(day from date) AS weekday
         FROM daily_data 
     ),
     add_more_features AS (
         SELECT *
     		, (CASE 
-    			WHEN month_name in ... THEN 'winter'
-    			WHEN ... THEN 'spring'
-                WHEN ... THEN 'summer'
-                WHEN ... THEN 'autumn'
+    			WHEN month_name in (12,1,2) THEN 'winter'
+    			WHEN month_name in (3,4,5 ) THEN 'spring'
+                WHEN month_name in (6,7,8) THEN 'summer'
+                WHEN month_name in (9,10,11) THEN 'autumn'
     		END) AS season
         FROM add_features
     )
