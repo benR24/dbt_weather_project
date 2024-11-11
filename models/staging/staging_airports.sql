b5acc1db1e97d3b1accbb75398ca3d0a17b972d0
benR24 <benedict.reymann@gmail.com>
2024-11-11 11:32:11 +0100

    commit3

---
+++ models/staging/staging_airports.sql
@@ -0,0 +1,8 @@
+sql
+    WITH airports_regions_join AS (
+        SELECT * 
+        FROM {{source('staging_flights', 'airports')}}
+        LEFT JOIN {{source('staging_flights', 'regions')}}
+        USING (country)
+    )
+    SELECT * FROM airports_regions_join
